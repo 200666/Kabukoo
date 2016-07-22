40ad925016c59bd889cdf956f29dcc8dd407d806
开文 童 <Tong@kevindeair.devices.brown.edu>
2016-07-22 11:16:11 -0400

    project

---
+++ Kabukoo/app/src/main/java/com/example/kabukoo/kabukoo/StartActivity.java
@@ -0,0 +1,17 @@
+package com.example.kabukoo.kabukoo;
+
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+       //Typeface myTypeFace = Typeface.createFromAsset(getAssets(),"catarataone.regular.ttf");
+        //TextView titleTextView = (TextView)findViewById(R.id.textView);
+        //titleTextView.setTypeFace(myTypeFace);
+    }
+}
