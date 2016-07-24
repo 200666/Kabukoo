06909724e28a5d67beecd929513dacab4d8846bf
Kevin Tong <Tong@kevindeair.devices.brown.edu>
2016-07-24 15:49:40 -0400

    1st commit

--- Kabukoo/app/src/main/java/com/example/kabukoo/kabukoo/StartActivity.java
+++ Kabukoo/app/src/main/java/com/example/kabukoo/kabukoo/StartActivity.java
@@ -1,17 +1,37 @@
 package com.example.kabukoo.kabukoo;
 
-import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.*;
+import android.view.MotionEvent;
+import android.view.View;
+import android.content.Context;
+import android.content.Intent;
+
 
 public class StartActivity extends AppCompatActivity {
+
+    Button myButton = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
-       //Typeface myTypeFace = Typeface.createFromAsset(getAssets(),"catarataone.regular.ttf");
-        //TextView titleTextView = (TextView)findViewById(R.id.textView);
-        //titleTextView.setTypeFace(myTypeFace);
+        //link to second page
+        myButton = (Button)findViewById(R.id.button);
+        myButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent myIntent = new Intent(v.getContext(), Kabame.class);
+                startActivity(myIntent);
+            }
+        });
     }
 }
+
+
